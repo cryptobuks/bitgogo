a2694633350195b4a7f1399960b6b61ef16d065d
Sebastian Kippe <sebastian@kip.pe>
2014-05-23 18:40:23 +0200

    Step 8 – Fetch wallet data

--- js/app.js
+++ js/app.js
@@ -2,11 +2,30 @@
 
 App = Ember.Application.create();
 
+App.reopen({
+  walletAddress: '1GXqRnh753Dtq99JAsfwRwg7XKDPENAZGV',
+  apiBaseUrl: 'http://insight.kip.pe/api/',
+  apiAddressUrl: function(){
+    return this.get('apiBaseUrl') + 'addr/' + this.get('walletAddress');
+  }.property('walletAddress', 'apiBaseUrl')
+});
+
 App.Router.map(function() {
   // put your routes here
 });
 
 App.IndexRoute = Ember.Route.extend({
+
+  setupController: function(controller) {
+    this._super();
+
+    Em.run(function(){
+      Em.$.getJSON(App.get('apiAddressUrl'), function(data){
+        console.log(data);
+      });
+    });
+  }
+
 });
 
 App.IndexController = Ember.Controller.extend({
